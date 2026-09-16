4fa4b5ad328fe3c7da6447e051e58dce2b6b03c9
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2026-09-17 00:57:43 +0200

    test reset clears index binding

--- renderer/tests/testRenderer.cpp
+++ renderer/tests/testRenderer.cpp
@@ -1010,6 +1010,37 @@ static void TestResetClearsBindings()
     assert(!commands->DrawTriangles(3));
 }
 
+static void TestResetClearsIndexBinding()
+{
+    myrenderer::Renderer renderer;
+
+    assert(renderer.Valid());
+
+    const myrenderer::Vertex vertices[3] = {
+        {10.0f, 10.0f, 0xFFFFFFFFu},
+        {30.0f, 10.0f, 0xFFFFFFFFu},
+        {20.0f, 30.0f, 0xFFFFFFFFu}};
+
+    const uint32_t indices[3] = {0, 1, 2};
+
+    auto vertexBuffer = renderer.CreateVertexBuffer(vertices, 3);
+    auto indexBuffer = renderer.CreateIndexBuffer(indices, 3);
+
+    assert(vertexBuffer != nullptr);
+    assert(indexBuffer != nullptr);
+
+    auto commands = renderer.CreateCommandBuffer(256);
+
+    assert(commands != nullptr);
+    assert(commands->BindVertexBuffer(*vertexBuffer));
+    assert(commands->BindIndexBuffer(*indexBuffer));
+    assert(commands->DrawIndexed(3));
+
+    assert(commands->Reset());
+
+    assert(!commands->DrawIndexed(3));
+}
+
 int main()
 {
     TestRendererBasics();
@@ -1064,6 +1095,7 @@ int main()
     TestCommandBufferResetCapacity();
 
     TestResetClearsBindings();
+    TestResetClearsIndexBinding();
 
     return 0;
 }
